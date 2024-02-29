321f14d0a2fa214d769a7e466d3a69694b519c77
LudoTreb <l.trebalag@gmail.com>
2024-02-29 17:31:17 +0100

    add the function for the settings, rendering and saving the image

--- calendar_automatic.py
+++ calendar_automatic.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import bpy
 
 # Paramètres
-
+render_output_path = Path(__file__).parent / "ress/image-test.jpg"
 # Chemin vers le fichier de police
 font_roman_path = (
     Path(__file__).parent / "ress/fonts/Helvetica LT Std/HelveticaLTStd-Roman.otf"
@@ -17,6 +17,20 @@ image_ref = Path(__file__).parent / "ress/ref/Calendar-2024_jan.png"
 year = 2024
 
 month = 1  # pour les essais Février 2024
+
+# Size canvas
+canvas_dim = (1.78, 2.502)
+canvas_pos = (0, 0, -0.001)
+
+# Setting render
+cam_pos = (0, 0, 6.967)
+scene_resolution = (3508, 4961)
+cam_ortho_scale = 5
+
+light_pos = (0, 0, 2)
+light_scale = (8, 8, 0)
+light_power = 5500
+
 
 # Paramètres de la grille de dates
 start_date_x = -1.35
@@ -46,28 +60,33 @@ month_pos_y = 1.420
 scale_month_factor = 0.8
 scale_month = (scale_month_factor, scale_month_factor, scale_month_factor)
 
+# Infos ligne de separation
+line_pos = (0, 0.9, 0)
+line_scale = (1.65, -0.003, 1)
+line_offset = 0.55
 
+# Info bloc noir
+bloc_pos = (0, 1.96, -0.0005)
+bloc_scale = (1.78, 0.54, 1)
+# TODO créer un fonction pour la création de material.
+# On aurait en parametre si mat=true alors pas specular ni roughness et juste le tuple rgb en diffuse
 # Les couleurs / material
 black_material = bpy.data.materials.new(name="Black")
 black_material.diffuse_color = (0, 0, 0, 1)
+black_material.specular_intensity = 0
+black_material.roughness = 0
 
 gray_material = bpy.data.materials.new(name="Gray")
-gray_material.diffuse_color = (0.5, 0.5, 0.5, 1)
+gray_material.diffuse_color = (0.05, 0.05, 0.05, 1)
+gray_material.specular_intensity = 0
+gray_material.roughness = 0
 
 white_material = bpy.data.materials.new(name="White")
 white_material.diffuse_color = (1, 1, 1, 1)
-
-# Size canvas
-canvas_dim_x = 1.78
-canvas_dim_y = 2.502
-canvas_pos_x = 0
-canvas_pos_y = 0
-canvas_pos_z = -0.1
-
-# les fonctions
+white_material.specular_intensity = 0
+white_material.roughness = 0
 
 
-#
 def create_day_line(start_day_x, start_day_y):
     """create the line of the day"""
     days = calendar.day_name
@@ -86,7 +105,24 @@ def create_day_line(start_day_x, start_day_y):
         x += cell_size
 
 
-def create_calendar_month(year, month, start_date_x, start_date_y, cell_size):
+def create_plane(mesh_pos: tuple, mesh_dim, color):
+    bpy.ops.mesh.primitive_plane_add(
+        size=2,
+        enter_editmode=False,
+        align="WORLD",
+        location=(mesh_pos[0], mesh_pos[1], mesh_pos[2]),
+        scale=(1, 1, 1),
+    )
+    backgroung = bpy.context.object
+    backgroung.active_material = color
+
+    backgroung.scale.x = mesh_dim[0]
+    backgroung.scale.y = mesh_dim[1]
+
+
+def create_calendar_month(
+    year, month, start_date_x, start_date_y, cell_size, item_one_pos
+):
     # Générer le calendrier pour le mois spécifié
     cal = calendar.Calendar().itermonthdates(year, month)
 
@@ -94,8 +130,8 @@ def create_calendar_month(year, month, start_date_x, start_date_y, cell_size):
     x, y = start_date_x, start_date_y
 
     final_x = start_date_x + 6 * cell_size
-
     # Boucle sur chaque jour du mois
+    line_pos = item_one_pos
     for dt in cal:
 
         bpy.ops.object.text_add(enter_editmode=False, location=(x, y, 0))
@@ -115,9 +151,15 @@ def create_calendar_month(year, month, start_date_x, start_date_y, cell_size):
         # Déplacer les coordonnées pour la prochaine case de la grille
         x += cell_size
         # Si nous atteignons la fin de la ligne, réinitialiser x et descendre à la ligne suivante
+
         if x > final_x:
+            create_plane(line_pos, line_scale, gray_material)
             x = start_date_x
             y -= cell_size + 0.08
+
+            temp_list = list(line_pos)
+            temp_list[1] -= line_offset
+            line_pos = tuple(temp_list)
 
 
 def create_month(month, month_pos_x, month_pos_y):
@@ -128,7 +170,7 @@ def create_month(month, month_pos_x, month_pos_y):
     bpy.context.object.data.body = str(months[month][:3])
 
     bpy.context.object.data.align_x = "LEFT"
-    bpy.context.object.active_material = black_material
+    bpy.context.object.active_material = white_material
     bpy.context.object.data.font = bpy.data.fonts.load(str(font_bold_path))
     bpy.context.object.scale = scale_month
 
@@ -138,31 +180,77 @@ def create_year(year, year_pos_x, year_pos_y):
     bpy.context.object.data.body = str(year)
 
     bpy.context.object.data.align_x = "RIGHT"
-    bpy.context.object.active_material = black_material
+    bpy.context.object.active_material = white_material
     bpy.context.object.data.font = bpy.data.fonts.load(str(font_roman_path))
     bpy.context.object.scale = scale_year
 
 
-def create_plane(pos_x, pos_y, pos_z, scale_x, scale_y, color):
-    bpy.ops.mesh.primitive_plane_add(
-        size=2,
+# TODO creer les différents éléments pour le rendu la cam et les setting
+def render_setting(
+    cam_pos, resolution, ortho_scale, light_pos, light_scale, light_power, output_path
+):
+    bpy.context.scene.render.image_settings.file_format = "JPEG"
+    bpy.context.scene.render.image_settings.quality = 100
+
+    bpy.ops.object.light_add(
+        type="AREA", align="WORLD", location=light_pos, scale=(1, 1, 1)
+    )
+    light = bpy.context.object
+    light.data.use_shadow = False
+    light.scale = light_scale
+    light.data.energy = light_power
+
+    bpy.context.scene.render.resolution_x = resolution[0]
+    bpy.context.scene.render.resolution_y = resolution[1]
+
+    bpy.ops.object.camera_add(
         enter_editmode=False,
         align="WORLD",
-        location=(pos_x, pos_y, pos_z),
+        location=cam_pos,
+        rotation=(0, 0, 0),
         scale=(1, 1, 1),
     )
-    backgroung = bpy.context.object
-    backgroung.active_material = white_material
+    camera = bpy.context.object
+    camera.data.type = "ORTHO"
+    camera.data.ortho_scale = ortho_scale
 
-    backgroung.scale.x = canvas_dim_x
-    backgroung.scale.y = canvas_dim_y
+    # Activer la vue de la caméra
+    bpy.context.scene.camera = camera
+
+    # Déclencher le rendu
+    bpy.ops.render.render(write_still=True)
+
+    # Sauvegarder l'image rendue
+    bpy.data.images["Render Result"].save_render(str(output_path))
+
+    # TODO automatiser les rendu de chaque planche --> jpeg
+    # TODO créer un pdf à partir de ces 12 jpeg et supprimer ces jpeg
+
+    # TODO Penser à comment bien organiser le code
+    """definir les dossiers 1 pour le script principale qui fera appel
+    aux  fonction dans le dossier constructor.
+    et fichier data (json) qui renseignera tous les info sur tous les élements
+    présent dans la scène, couleur, position, scale des objets, les setting de rendu et autres
+    """
 
 
 bpy.ops.object.select_all(action="SELECT")
 bpy.ops.object.delete(use_global=False, confirm=False)
 
-create_objet()
+create_plane(canvas_pos, canvas_dim, white_material)
+create_plane(bloc_pos, bloc_scale, black_material)
+
 create_year(year, year_pos_x, year_pos_y)
 create_month(1, month_pos_x, month_pos_y)
 create_day_line(start_day_x, start_day_y)
-create_calendar_month(year, month, start_date_x, start_date_y, cell_size)
+create_calendar_month(year, month, start_date_x, start_date_y, cell_size, line_pos)
+
+render_setting(
+    cam_pos,
+    scene_resolution,
+    cam_ortho_scale,
+    light_pos,
+    light_scale,
+    light_power,
+    render_output_path,
+)
