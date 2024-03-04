d55145fa5e16493cffbcf2b8c2ded0901338687b
LudoTreb <l.trebalag@gmail.com>
2024-03-04 21:37:27 +0100

    Add the function to create a pdf from the 12 jpg. And delete the folder of jpeg after.

--- calendar_automatic.py
+++ calendar_automatic.py
@@ -3,9 +3,7 @@ from pathlib import Path
 
 import bpy
 
-# Paramètres
-render_output_path = Path(__file__).parent / "ress/image-test.jpg"
-# Chemin vers le fichier de police
+# Path for the font
 font_roman_path = (
     Path(__file__).parent / "ress/fonts/Helvetica LT Std/HelveticaLTStd-Roman.otf"
 )
@@ -37,63 +35,40 @@ light_scale = (8, 8, 0)
 light_power = 5500
 
 
-# Paramètres de la grille de dates
-start_date_x = -1.35
-start_date_y = 0.68
-scale_date = (0.2, 0.2, 0.2)
+# Parameter of the date's grid
+date_pos = (-1.35, 0.68, 0)
+date_scale = (0.2, 0.2, 0.2)
 
-cell_size = 0.46  # Taille de la cellule de la grille
+# Size of a grid's cellule
+cell_size = 0.46
 
-# Paramètres de la grille des jours
+# Parameters of the day's line
 day_pos = (-1.35, 1, 0)
 day_scale = (0.10, 0.10, 0.10)
 
-start_day_x = -1.35
-start_day_y = 1
-scale_day = (0.10, 0.10, 0.10)
-
-# Paramètres de la grille des barres
-start_line_x = -0.5
-start_line_y = -0.5
-
-# Position de l'année
-
-
-# Position du mois
+# Parameters of month
 month_pos = (-1.45, 1.9, 0)
 month_pos_2 = (-1.45, 1.9, -0.0006)
 month_scale = (0.8, 0.8, 0.8)
 
-month_pos_x = -1.45
-month_pos_y = 1.420
-scale_month_factor = 0.8
-scale_month = (scale_month_factor, scale_month_factor, scale_month_factor)
-
-# Infos ligne de separation
+# Parameters of separate's line
 line_pos = (0, 0.9, 0)
 line_scale = (1.65, -0.003, 1)
 line_offset = 0.55
 
-# Info bloc noir
+# Parameter of the black plane
 bloc_pos = (0, 1.9095, -0.0005)
 bloc_scale = (1.78, 0.631, 1)
-# TODO créer un fonction pour la création de material.
-# On aurait en parametre si mat=true alors pas specular ni roughness et juste le tuple rgb en diffuse
-# Les couleurs / material
-black_material = bpy.data.materials.new(name="Black")
-black_material.diffuse_color = (0, 0, 0, 1)
-black_material.specular_intensity = 0
-black_material.roughness = 0
 
-gray_material = bpy.data.materials.new(name="Gray")
-gray_material.diffuse_color = (0.05, 0.05, 0.05, 1)
-gray_material.specular_intensity = 0
-gray_material.roughness = 0
 
-white_material = bpy.data.materials.new(name="White")
-white_material.diffuse_color = (1, 1, 1, 1)
-white_material.specular_intensity = 0
-white_material.roughness = 0
+def create_material(
+    name: str, diffuse_color: tuple, specular_intensity: int, roughness: int
+):
+    material = bpy.data.materials.new(name=name)
+    material.diffuse_color = diffuse_color
+    material.specular_intensity = specular_intensity
+    material.roughness = roughness
+    return material
 
 
 def create_text(
@@ -142,16 +117,14 @@ def create_plane(mesh_pos: tuple, mesh_dim, color):
     backgroung.scale.y = mesh_dim[1]
 
 
-def create_calendar_month(
-    year, month, start_date_x, start_date_y, cell_size, item_one_pos
-):
+def create_calendar_month(year, month, date_pos, date_scale, cell_size, item_one_pos):
     # Générer le calendrier pour le mois spécifié
     cal = calendar.Calendar().itermonthdates(year, month)
 
     # Coordonnées de départ pour la première case de la grille
-    x, y = start_date_x, start_date_y
+    x, y = date_pos[0], date_pos[1]
 
-    final_x = start_date_x + 6 * cell_size
+    final_x = x + 6 * cell_size
     # Boucle sur chaque jour du mois
     line_pos = item_one_pos
     for dt in cal:
@@ -168,7 +141,7 @@ def create_calendar_month(
         else:
             day_text.active_material = gray_material
 
-        day_text.scale = scale_date  # Ajuster la taille du texte
+        day_text.scale = date_scale  # Ajuster la taille du texte
 
         # Déplacer les coordonnées pour la prochaine case de la grille
         x += cell_size
@@ -176,7 +149,7 @@ def create_calendar_month(
 
         if x > final_x:
             create_plane(line_pos, line_scale, gray_material)
-            x = start_date_x
+            x = date_pos[0]
             y -= cell_size + 0.08
 
             temp_list = list(line_pos)
@@ -222,27 +195,100 @@ def render_setting(
     bpy.data.images["Render Result"].save_render(str(output_path))
 
 
-# TODO créer un pdf à partir de ces 12 jpeg et supprimer ces jpeg
+def delete_temp_folder(path_folder_to_delete):
+    """Before delete the folder, it's checking if the folder name content temp
+
+    Args:
+        path_folder_to_delete (_type_): _description_
+    """
+    if (
+        path_folder_to_delete.exists()
+        and path_folder_to_delete.is_dir()
+        and ("temp" in str(path_folder_to_delete))
+    ):
+        for item in path_folder_to_delete.iterdir():
+            if item.is_file():
+                item.unlink()
+
+        path_folder_to_delete.rmdir()
+
+    else:
+        print("Le dossier n'existe pas")
+
+
+def create_pdf_from_img(name_pdf: str, path_imgs_folder: str = "ress/temp_img"):
+    """create one pdf from all images present in a list.
+
+    Args:
+        paht_imgs_folder (_type_): Path object of the images folder
+        path_pdf_output (_type_): Path objet of the pdf file
+    """
+    # récupère sous une string tous les chemins des images jpeg dans une liste
+    image_folder = Path(__file__).parent / path_imgs_folder
+    image_paths = image_folder.glob("*.jpg")
+
+    # convertis tous ces jpeg en pdf
+    for image in image_paths:
+        img = Image.open(image, "r")
+        img_convert = img.convert("RGB")
+        img_convert.save(f"{str(image)[:-3]}pdf")
+
+    # Récupère sous une string tous les chemins des images pdf dans une liste
+    pdf_paths = image_folder.glob("*.pdf")
+    # Tri les pdf en ordre croissant par rapport à leur nom qui fini par un nombre
+    pdf_paths_sorted = sorted(
+        pdf_paths, key=lambda path: int(str(path).split("_")[-1].split(".")[0])
+    )
+    # créer une instance de Pdfwritter pour qui va permmettre de fusionner les pdf en un seul
+    merger = PdfWriter()
+    pdf_out_folder = Path(__file__).parent / "export"
+    pdf_out_folder.mkdir()
+    pdf_out_file = pdf_out_folder / name_pdf
+    # fusionne les pdf en un seul
+    for pdf in pdf_paths_sorted:
+        merger.append(pdf)
+    merger.write(pdf_out_file)
+    merger.close
+
+    # supprime le dossier temp
+    delete_temp_folder(image_folder)
+
 
 # TODO Penser à comment bien organiser le code
-"""definir les dossiers 1 pour le script principale qui fera appel
+"""
+Definir les dossiers 1 pour le script principale qui fera appel
 aux  fonction dans le dossier constructor.
 et fichier data (json) qui renseignera tous les info sur tous les élements
 présent dans la scène, couleur, position, scale des objets, les setting de rendu et autres
 """
 
 
-render_output_folder = Path(__file__).parent / "ress/images"
+name_calendar = "calendar_graphictypo_2024.pdf"
+render_output_folder = Path(__file__).parent / "ress"
 months = calendar.month_name
 days = calendar.day_name
 
-for month_num in range(1, 13):
+black_material = create_material(
+    "Black", diffuse_color=(0, 0, 0, 1), specular_intensity=0, roughness=0
+)
+gray_material = create_material(
+    "Gray", diffuse_color=(0.05, 0.05, 0.05, 1), specular_intensity=0, roughness=0
+)
+white_material = create_material(
+    "White", diffuse_color=(1, 1, 1, 1), specular_intensity=0, roughness=0
+)
+
+
+for month_num in range(1, 3):
     bpy.ops.object.select_all(action="SELECT")
     bpy.ops.object.delete(use_global=False, confirm=False)
-    # définir le chemin pour l'enregistrement
-    render_output_path = (
-        render_output_folder / f"calendar_graphictypo_{year}_{month_num}.jpg"
-    )
+
+    # create the path of the folder temporary save the image jpeg
+    temp_img_path = render_output_folder / "temp_img"
+    if not temp_img_path.is_dir():
+        temp_img_path.mkdir()
+
+    render_output_path = temp_img_path / f"calendar_graphictypo_{year}_{month_num}.jpg"
 
     # create the white backgound
     create_plane(canvas_pos, canvas_dim, white_material)
@@ -252,7 +298,12 @@ for month_num in range(1, 13):
 
     # create the text year
     create_text(
-        year, year_pos, white_material, font_roman, year_scale, align=("RIGHT", "TOP")
+        year,
+        year_pos,
+        white_material,
+        font_roman,
+        year_scale,
+        align=("RIGHT", "TOP"),
     )
 
     # create the month
@@ -277,9 +328,7 @@ for month_num in range(1, 13):
         day_pos_init = tuple(day_pos_temp)
 
     # create all date of the month
-    create_calendar_month(
-        year, month_num, start_date_x, start_date_y, cell_size, line_pos
-    )
+    create_calendar_month(year, month_num, date_pos, date_scale, cell_size, line_pos)
 
     # render & save image
     render_setting(
@@ -291,3 +340,14 @@ for month_num in range(1, 13):
         light_power,
         render_output_path,
     )
+
+
+# TODO Trouver une solution pour lancer le script en une fois sans avoir à installer Pillow dans l'environnement de blender.
+"""Une solution serait dans le script d'avoir une ligne qui lance la partie blender, l'execute et puis l'arrete. 
+Puis on continue le script sur la création du pdf.
+"""
+
+# from PIL import Image
+# from PyPDF2 import PdfWriter
+
+# create_pdf_from_img(name_calendar)
