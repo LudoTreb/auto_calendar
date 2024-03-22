164fce278359ccdc2945aca96d97f99709ecaff3
LudoTreb <l.trebalag@gmail.com>
2024-03-22 12:04:44 +0100

    Refactoring: delete useless variable and comment. REname properly some variable and function.

--- main.py
+++ main.py
@@ -8,32 +8,23 @@ with open("data.json", "r") as json_file:
     data = json.load(json_file)
 
 name_calendar = data["Settings"]["save"]["name"]
+my_script = "calendar_automatic.py"
 
 
-def lancer_script_blender(nom_fichier_blend, nom_script):
-    # Chemin absolu du fichier Blender
-    chemin_fichier_blend = Path(__file__).parent / nom_fichier_blend
+def run_blender_script(my_script):
 
-    # Spécifier le chemin complet de l'exécutable Blender
-    chemin_blender = "/Applications/Blender.app/Contents/MacOS/Blender"
+    executable_blender_path = "/Applications/Blender.app/Contents/MacOS/Blender"
 
-    # Construire la commande pour lancer Blender avec le script en arrière-plan
-    commande_blender = [
-        chemin_blender,  # Chemin complet de l'exécutable Blender
-        "--background",  # Lancer Blender en arrière-plan sans interface graphique
+    command_blender = [
+        executable_blender_path,
+        "--background",
         "--python",
-        nom_script,  # Spécifier le script Python à exécuter
+        my_script,
     ]
 
-    # Lancer le processus Blender avec le script en arrière-plan
-    subprocess.run(commande_blender, check=True)
+    subprocess.run(command_blender, check=True)
 
 
-# Nom du fichier Blender et nom du script à exécuter
-nom_fichier_blend = "calendar-2024.blend"
-nom_script = "calendar_automatic.py"
-
-# Appeler la fonction pour lancer le script Blender en arrière-plan
-lancer_script_blender(nom_fichier_blend, nom_script)
+run_blender_script(my_script)
 
 create_pdf_from_img(name_calendar)
